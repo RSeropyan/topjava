7c7f1da9205f43bbdcf666dbcfc56147a9a1ed3f
Roman Seropyan <seropyanry@gmail.com>
2020-10-13 23:08:51 +0300

    MealRestController small refactoring

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
@@ -9,6 +8,9 @@ import ru.javawebinar.topjava.util.MealsUtil;
 import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
 @Controller
 public class MealRestController {
@@ -20,13 +22,13 @@ public class MealRestController {
     }
 
     public Meal create(Meal meal) {
+        checkNew(meal);
         return service.save(meal, SecurityUtil.authUserId());
     }
 
     public void update(Meal meal, int id) {
-        if (meal.getId() == id) {
-            service.save(meal, SecurityUtil.authUserId());
-        }
+        assureIdConsistent(meal, id);
+        service.save(meal, SecurityUtil.authUserId());
     }
 
     public void deleteById(Integer id) {
