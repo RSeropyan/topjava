19c417b17e75a756a0aa8d35e898250ac3c27183
Roman Seropyan <seropyanry@gmail.com>
2020-11-16 21:56:11 +0300

    Jdbc tests repaired

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,10 +1,13 @@
 package ru.javawebinar.topjava.service;
 
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
@@ -24,6 +27,22 @@ import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 abstract public class AbstractServiceTest {
+
+    @Autowired
+    private Environment environment;
+
+    protected boolean isJdbcProfileActive = false;
+
+    @Before
+    public void checkIfJdbcRepositoryImplementationActive() throws Exception {
+        isJdbcProfileActive = false;
+        for (String profileName : environment.getActiveProfiles()) {
+            if (profileName.equals("jdbc")) {
+                isJdbcProfileActive = true;
+                break;
+            }
+        }
+    }
 
     @ClassRule
     public static ExternalResource summary = TimingRules.SUMMARY;
